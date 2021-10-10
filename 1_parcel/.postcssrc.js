a793948be0defd5e8090410cc2e7aa963b62f5f3
kkojae91 <jaejeung3210@gmail.com>
2021-10-10 16:39:58 +0900

    autoprefixer, babel 설치 및 적용

---
+++ 1_parcel/.postcssrc.js
@@ -0,0 +1,14 @@
+// ESM (EcmaScript Modules)
+// CommonJS
+
+// // import autoprefixer from 'autoprefixer';
+// const autoprefixer = require("autoprefixer");
+
+// // export = {plugins: [autoprefixer]}
+// module.exports = {
+//   plugins: [autoprefixer],
+// };
+
+module.exports = {
+  plugins: [require("autoprefixer")],
+};
